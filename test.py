11e6d4ea7a87013fcde1dd87f800e653c676bfe8
Maxim Bodrov <activer12@mail.ru>
2022-07-18 14:30:51 +0300

    0.0.3

--- test.py
+++ test.py
@@ -1,16 +1,21 @@
 from functions import *
 from progress import *
+from mycolor import *
 from userinp import UserInp
 
 # ! Тестовая зона 1
 
-#a = UserInp()
-#a.run()
-#print('Just stoped')
+# a = UserInp()
+# a.run()
+# print('Just stoped')
 
 # ! Тестовая зона 2
 
 # for i in range(100):
 #    base_progress(i, 100, 30)
+
+# ! Тестовая зона 2
+
+print('Hello', setst(' Me ', (21, 5, 47, 30)), '!!!')
 
 # ! Конец)))
